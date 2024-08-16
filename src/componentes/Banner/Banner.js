0b70e41839e25e6a97044eab9ea605eb8f839e7c
Giulia <giulia.floripes@gmail.com>
2024-08-15 22:53:22 -0300

    organo v1

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className='banner'>
+            <img src='/imagens/imagens/imagens/banner.png' alt='O banner da página do Organo'/>
+        </header>
+    )
+}
+export default Banner 
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,25 @@
+import './Rodape.css';
+
+const Rodape = () => {
+    return (
+
+        <section className='rodape'>
+            <div className='rede-sociais'>
+                <img src='/imagens/imagens/imagens/fb.png' alt='Logo do facebook'/>
+                <img src='/imagens/imagens/imagens/tw.png' alt='Logo do twitter'/>
+                <img src='/imagens/imagens/imagens/ig.png' alt='Logo do instagram'/>
+            </div>
+
+            <div className='organo'>
+                <img src='/imagens/imagens/imagens/logo.png' alt='Logo do Organo'/>
+            </div>
+
+            <div className='paragrafo'>
+                Desenvolvido por Giulia
+            </div>
+
+        </section>
+    )
+}
+
+export default Rodape 
